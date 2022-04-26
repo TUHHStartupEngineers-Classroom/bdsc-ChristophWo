68f93ee1c6bde2e41a38e7bf70d4ee919012b36b
ChristophWo <christoph.wolter@tuhh.de>
2022-04-26 17:53:31 +0200

    started competitor data challenge

---
+++ 02_data_acquisition_files/competitor_data_challenge.R
@@ -0,0 +1,33 @@
+
+  
+  # WEBSCRAPING ----
+
+# 1.0 LIBRARIES ----
+
+library(tidyverse) # Main Package - Loads dplyr, purrr, etc.
+library(rvest)     # HTML Hacking & Web Scraping
+library(xopen)     # Quickly opening URLs
+library(jsonlite)  # converts JSON files to R objects
+library(glue)      # concatenate strings
+library(stringi)   # character string/text processing
+
+# 1.1 COLLECT PRODUCT FAMILIES ----
+
+url_home          <- "https://www.rosebikes.de/"
+#xopen(url_home) # Open links directly from RStudio to inspect them
+
+# Read in the HTML for the entire webpage
+html_home         <- read_html(url_home)
+
+# Web scrape the ids for the families
+bike_family_tbl <- html_home %>%
+  
+  # Get the nodes for the families ...
+  html_nodes(css = ".js-navigationDrawer__list--secondary") %>%
+  # ...and extract the information of the id attribute
+  html_attr('id') %>%
+  
+  # Remove the product families Gear and Outlet and Woman 
+  # (because the female bikes are also listed with the others)
+  discard(.p = ~stringr::str_detect(.x,"WMN|WOMEN|GEAR|OUTLET")) %>%
+  
