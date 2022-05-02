f0d7356cb58c11b9534c50caad778cf663a587e9
ChristophWo <49435772+ChristophWo@users.noreply.github.com>
2022-05-02 18:41:40 +0200

    data_wrangling_challenge

--- 03_data_wrangling_files/data_wrangling_challenge.R
+++ 03_data_wrangling_files/data_wrangling_challenge.R
@@ -1,5 +1,8 @@
 library(vroom)
-col_types <- list(
+
+
+#getting patent data
+col_types_patent <- list(
   id = col_character(),
   type = col_character(),
   number = col_character(),
@@ -14,10 +17,54 @@ col_types <- list(
 )
 
 patent_tbl <- vroom(
-  file       = "03_data_wrangling_files/patent_data/patent.tsv", 
+  file       = "C:/Users/CWolt/Documents/patent_data/patent.tsv", 
   delim      = "\t", 
-  col_types  = col_types,
+  col_types  = col_types_patent,
   na         = c("", "NA", "NULL")
 )
 
-new_table<-select(continent,date)
+#getting assignee data
+col_types_assignee <- list(
+  id = col_character(),
+  type = col_character(),
+  organization = col_character()
+  )
+
+assignee_tbl <- vroom(
+  file       = "C:/Users/CWolt/Documents/patent_data/assignee.tsv", 
+  delim      = "\t", 
+  col_types  = col_types_assignee,
+  na         = c("", "NA", "NULL")
+)
+
+#getting patent assignee data to connect patent and assignee data
+
+col_types_patent_assignee <- list(
+  patent_id = col_character(),
+  assignee_id = col_character()
+)
+
+patent_assignee_tbl <- vroom(
+  file       = "C:/Users/CWolt/Documents/patent_data/patent_assignee.tsv", 
+  delim      = "\t", 
+  col_types  = col_types_patent_assignee,
+  na         = c("", "NA", "NULL")
+)
+
+col_types_uspc <- list(
+  patent_id = col_character(),
+  mainclass_id = col_character()
+)
+
+uspc_tbl <- vroom(
+  file       = "C:/Users/CWolt/Documents/patent_data/uspc.tsv", 
+  delim      = "\t", 
+  col_types  = col_types_uspc,
+  na         = c("", "NA", "NULL")
+)
+
+
+patent_joined <- left_join( patent_tbl, patent_assignee_tbl, by = c("id" = "patent_id"))
+patent_joined <- left_join( patent_joined, assignee_tbl, by = c("assignee_id" = "id"))
+patent_joined <- left_join( patent_joined, uspc_tbl, by = c("id" = "patent_id"))
+#new_table<-select(continent,date) #for data visualization
