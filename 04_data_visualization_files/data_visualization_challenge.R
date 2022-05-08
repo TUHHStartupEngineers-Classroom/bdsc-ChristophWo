8f1d01f1bb385008030406f2ddb4a96fc1f4ec2b
ChristophWo <49435772+ChristophWo@users.noreply.github.com>
2022-05-08 14:00:03 +0200

    Update data_visualization_challenge.R

--- 04_data_visualization_files/data_visualization_challenge.R
+++ 04_data_visualization_files/data_visualization_challenge.R
@@ -4,6 +4,15 @@
 library(tidyverse)
 library(ggrepel)
 library(ggplot2)
+library(maps)
+library(maptools)
+library(mapdata)
+library(ggthemes)
+library(tibble)
+library(viridis)
+library(mapproj)
+
+
 covid_data_tbl <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
 
 covid_data_selected_tbl <- covid_data_tbl  %>% 
@@ -20,10 +29,35 @@ covid_data_selected_tbl%>%
   scale_color_manual(values = c("#1b98e0", "#353435","#FF0000", "#00FF00","#FF00FF"))
   
 
+#Challenge 2
 
 covid_data_selected_tbl <- covid_data_tbl  %>% 
-  select(continent,location,date,total_cases)
+  select(continent,location,date,total_cases, total_deaths,population)
+
+covid_data_selected_tbl <- covid_data_selected_tbl %>%
+  mutate(mortality_rate = (total_deaths/population)*100)
+
+covid_data_selected_tbl <- covid_data_selected_tbl %>% filter(date == c("2022-05-07")) #filter by date
+covid_data_selected_tbl <- covid_data_selected_tbl %>%  rename("region" = location)
+covid_data_selected_tbl <- covid_data_selected_tbl %>%  select(region,mortality_rate)
+
+plot_data <- data_frame(region=unique(world$region))
+plot_data <- left_join( plot_data, covid_data_selected_tbl, by = c("region" = "region"))
+
+world <- map_data("world")
 
 
-plot_data %>% ggplot( ... ) +
-  geom_map(aes(map_id = ..., ... ), map = world, ... ) +
+gg <- ggplot()
+gg <- gg + geom_map(data=world, map=world,
+                    aes(long, lat, map_id=region),
+                    color="#2b2b2b", fill=NA, size=0.15)
+gg <- gg + geom_map(data=plot_data, map=world,
+                    aes(fill=mortality_rate,
+                        map_id=region),
+                    color="white", size=0.15)
+gg <- gg + scale_fill_viridis(name="Mortality Rate")
+gg <- gg + coord_map("polyconic")
+gg <- gg + theme_map()
+gg <- gg + theme(plot.margin=margin(20,20,20,20))
+gg <- gg + theme(legend.position=c(0.85, 0.2))
+gg
